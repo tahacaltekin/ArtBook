e6439fd71168dbaac41318bc8d0d6a302a46f86d
tahacaltekin <thcaltekin@gmail.com>
2021-10-14 17:45:21 +0300

    ArtBook

---
+++ app/src/main/java/com/taha/artbook/Art.java
@@ -0,0 +1,12 @@
+package com.taha.artbook;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
